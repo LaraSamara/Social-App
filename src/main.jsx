88c5272d75d794b4c72922d91adab6a70f3f5120
LaraSamara <larasamara2002@gmail.com>
2025-09-24 22:54:06 +0300

    Fix import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import { PostContextProvider } from './Context/PostContext.jsx';
 import { Toaster } from 'react-hot-toast';
-import { TokenContextProvider } from './Context/TokenContext.jsx';
+import { TokenContextProvider } from './Context/tokenContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <TokenContextProvider>
